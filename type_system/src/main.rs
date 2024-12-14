a81c949e4b15eb3c7257ffdc741feb57bf9cc250
Olivier Lasne <lasneolivier@gmail.com>
2024-12-14 19:22:10 +0100

    type_system: added percentage

--- type_system/src/main.rs
+++ type_system/src/main.rs
@@ -1,8 +1,7 @@
 use std::fmt;
-use std::ops::Add;
+use std::ops::{Add,Mul};
 
 // Currency Type
-#[allow(unused)]
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
 enum Currency {
     Euros,
@@ -67,6 +66,28 @@ impl Add<Money> for Money {
     }
 }
 
+// Implement Percentage operations
+impl Add<Percentage> for Money {
+    type Output = Money;
+
+    fn add(self, rhs: Percentage) -> Self::Output {
+        Money {
+            amount: self.amount + (self.amount * rhs.value / 100.0),
+            currency: self.currency
+        }
+    }
+}
+
+impl Mul<Percentage> for Money {
+    type Output = Money;
+
+    fn mul(self, rhs: Percentage) -> Self::Output {
+        Money {
+            amount: self.amount * rhs.value / 100.0,
+            currency: self.currency
+        }
+    }
+}
 
 impl fmt::Display for Money {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -77,11 +98,44 @@ impl fmt::Display for Money {
     }
 }
 
+// Percentage
+struct Percentage {
+    value: f64
+}
+
+impl Percentage {
+    pub fn new(value: f64) -> Percentage {
+        Percentage { value: value}
+    }
+}
+
+impl fmt::Display for Percentage {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:.1}%", self.value)
+    }
+}
+
+// Implement operations for Percent
+
+impl Add<Percentage> for Percentage {
+    type Output = Percentage;
+
+    fn add(self, rhs: Percentage) -> Self::Output {
+        Percentage::new(self.value + rhs.value)
+    }
+}
+
+
 fn main() {
     let alice = Money::new(42.0, Currency::Euros);
     let bob = Money::new(1337.0, Currency::Dollars);
-
     println! ("Alice & Bob have {}", alice + bob);
 
-    println!("Alice has {}", alice.conversion(Currency::Dollars));
+    let pourcent = Percentage::new(15.0) + Percentage::new(22.0);
+    println!("Pourcent: {}", pourcent);
+
+
+    println!("42€ + 12%: {}", Money::new(42.0, Currency::Euros) + Percentage::new(12.0));
+
+    println!("12% of 42€: {}", Money::new(42.0, Currency::Euros) * Percentage::new(12.0));
 }
