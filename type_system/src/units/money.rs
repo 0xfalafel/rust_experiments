522b7dd320b3141375391de9c049867f555a521c
Olivier Lasne <lasneolivier@gmail.com>
2024-12-14 19:50:13 +0100

    type_system: split units in multiple files

---
+++ type_system/src/units/money.rs
@@ -0,0 +1,101 @@
+use std::fmt;
+use std::ops::{Add, Mul};
+
+use crate::Percentage;
+
+// Currency Type
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub enum Currency {
+    Euros,
+    Dollars
+}
+
+impl fmt::Display for Currency {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let symbol = match self {
+            Currency::Euros => '€',
+            Currency::Dollars => '$',
+        };
+        write!(f, "{}", symbol)
+    }
+}
+
+
+// Money Type
+#[derive(Debug, PartialEq, Clone, Copy)]
+
+pub struct Money {
+    pub amount: f64,
+    pub currency: Currency
+}
+
+impl Money {
+    pub fn new(amount: f64, currency: Currency) -> Money {
+        Money {amount: amount, currency: currency}
+    }
+
+    fn conversion(self, new_currency: Currency) -> Money {
+        if self.currency == new_currency {
+            return self;
+        }
+
+        if self.currency == Currency::Euros && new_currency == Currency::Dollars {
+            Money::new(self.amount * 1.05, new_currency)
+            
+        } else if self.currency == Currency::Dollars && new_currency == Currency::Euros {
+            Money::new(self.amount / 1.05, new_currency)
+            
+        } else {
+            unreachable!("Fuck, we need to thing about conversion now :(")
+        }
+    }
+}
+
+impl Add<f64> for Money {
+    type Output = Money;
+
+    fn add(self, rhs: f64) -> Self::Output {
+        Money::new(self.amount + rhs, self.currency)
+    }
+}
+
+impl Add<Money> for Money {
+    type Output = Money;
+
+    fn add(self, other: Money) -> Self::Output {
+        let other = other.conversion(self.currency);
+        Money::new(self.amount + other.amount, self.currency)
+    }
+}
+
+// Implement Percentage operations
+impl Add<Percentage> for Money {
+    type Output = Money;
+
+    fn add(self, rhs: Percentage) -> Self::Output {
+        Money {
+            amount: self.amount + (self.amount * rhs.value / 100.0),
+            currency: self.currency
+        }
+    }
+}
+
+impl Mul<Percentage> for Money {
+    type Output = Money;
+
+    fn mul(self, rhs: Percentage) -> Self::Output {
+        Money {
+            amount: self.amount * rhs.value / 100.0,
+            currency: self.currency
+        }
+    }
+}
+
+impl fmt::Display for Money {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.currency {
+            Currency::Dollars => write!(f, "${:.2}", self.amount),
+            Currency::Euros => write!(f, "{:.2}€", self.amount)         
+        }
+    }
+}
---
+++ type_system/src/units/percentage.rs
@@ -0,0 +1,30 @@
+use std::fmt;
+use std::ops::Add;
+
+// Percentage
+#[derive(Debug, PartialEq)]
+pub struct Percentage {
+    pub value: f64
+}
+
+impl Percentage {
+    pub fn new(value: f64) -> Percentage {
+        Percentage { value: value}
+    }
+}
+
+impl fmt::Display for Percentage {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:.1}%", self.value)
+    }
+}
+
+// Implement operations for Percent
+
+impl Add<Percentage> for Percentage {
+    type Output = Percentage;
+
+    fn add(self, rhs: Percentage) -> Self::Output {
+        Percentage::new(self.value + rhs.value)
+    }
+}
