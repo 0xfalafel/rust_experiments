8443173ec743183fb2ebca40a3faf5805c0fad6c
Olivier Lasne <lasneolivier@gmail.com>
2024-12-14 18:59:58 +0100

    type_system: implemented money conversion + add

--- type_system/src/main.rs
+++ type_system/src/main.rs
@@ -1,7 +1,9 @@
 use std::fmt;
+use std::ops::Add;
 
 // Currency Type
 #[allow(unused)]
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 enum Currency {
     Euros,
     Dollars
@@ -19,6 +21,7 @@ impl fmt::Display for Currency {
 
 
 // Money Type
+#[derive(Debug, PartialEq, Clone, Copy)]
 
 struct Money {
     amount: f64,
@@ -29,18 +32,56 @@ impl Money {
     pub fn new(amount: f64, currency: Currency) -> Money {
         Money {amount: amount, currency: currency}
     }
+
+    fn conversion(self, new_currency: Currency) -> Money {
+        if self.currency == new_currency {
+            return self;
+        }
+
+        if self.currency == Currency::Euros && new_currency == Currency::Dollars {
+            Money::new(self.amount * 1.05, new_currency)
+            
+        } else if self.currency == Currency::Dollars && new_currency == Currency::Euros {
+            Money::new(self.amount / 1.05, new_currency)
+            
+        } else {
+            unreachable!("Fuck, we need to thing about conversion now :(")
+        }
+    }
 }
+
+impl Add<f64> for Money {
+    type Output = Money;
+
+    fn add(self, rhs: f64) -> Self::Output {
+        Money::new(self.amount + rhs, self.currency)
+    }
+}
+
+impl Add<Money> for Money {
+    type Output = Money;
+
+    fn add(self, other: Money) -> Self::Output {
+        let other = other.conversion(self.currency);
+        Money::new(self.amount + other.amount, self.currency)
+    }
+}
+
 
 impl fmt::Display for Money {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.currency {
-            Currency::Dollars => write!(f, "${}", self.amount),
-            Currency::Euros => write!(f, "{}€", self.amount)         
+            Currency::Dollars => write!(f, "${:.2}", self.amount),
+            Currency::Euros => write!(f, "{:.2}€", self.amount)         
         }
     }
 }
 
 fn main() {
     let alice = Money::new(42.0, Currency::Euros);
-    println! ("Alice has {}", alice);
+    let bob = Money::new(1337.0, Currency::Dollars);
+
+    println! ("Alice & Bob have {}", alice + bob);
+
+    println!("Alice has {}", alice.conversion(Currency::Dollars));
 }
