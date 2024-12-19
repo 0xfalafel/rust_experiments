6d11990611837c2240e686f8b0cecd91711d10eb
Lasne Olivier <lasneolivier@gmail.com>
2024-12-19 12:05:42 +0100

    used a macro to implement multiple operations in percentage

--- type_system/src/units/percentage.rs
+++ type_system/src/units/percentage.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::ops::{Add, Mul};
+use std::ops::{Add, Sub, Mul, Div};
 
 // Percentage
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -21,6 +21,20 @@ impl fmt::Display for Percentage {
 
 // Implement operations for Percent
 
+macro_rules! impl_arithmetic_op_for_Percentage {
+    ($trait_name:ident $fn_name:ident $op:tt) => {
+        
+        impl $trait_name<Percentage> for Percentage {
+            type Output = Percentage;
+        
+            fn $fn_name(self, rhs: Percentage) -> Self::Output {
+                Percentage::new(self.value $op rhs.value)
+            }
+        }                
+    };
+}
+
+/*
 impl Add<Percentage> for Percentage {
     type Output = Percentage;
 
@@ -28,6 +42,12 @@ impl Add<Percentage> for Percentage {
         Percentage::new(self.value + rhs.value)
     }
 }
+*/
+impl_arithmetic_op_for_Percentage!(Add add +);
+impl_arithmetic_op_for_Percentage!(Sub sub -);
+impl_arithmetic_op_for_Percentage!(Mul mul *);
+impl_arithmetic_op_for_Percentage!(Div div /);
+
 
 impl Add<f64> for Percentage {
     type Output = f64;
@@ -98,4 +118,18 @@ mod tests {
     fn add() {
         assert_eq!(Percentage::new(15.0) + Percentage::new(22.0), Percentage { value: 37.0});
     }
+
+    #[test]
+    fn sub() {
+        assert_eq!(Percentage::new(15.0) - Percentage::new(22.0), Percentage { value: -7.0});
+    }
+
+    #[test]
+    fn mul() { assert_eq!(Percentage::new(-7.0) * Percentage::new(-2.0), Percentage { value: 14.0})}
+
+    #[test]
+    fn div() { assert_eq!(Percentage::new(13.0) / Percentage::new(2.0), Percentage { value: 6.5})}
+
+    // #[test]
+    // fn div_zero() { assert_eq!(Percentage::new(13.0) / Percentage::new(0.0), Percentage { value: 0.0})}
 }
